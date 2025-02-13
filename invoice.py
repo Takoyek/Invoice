f6b9f57d9568fe99a51905e7d4a591b24201fa42
Saeid <saeid.barati.t3@gmail.com>
2025-02-14 02:49:27 +0330

    More than 30 days Version: 2.0

--- invoice.py
+++ invoice.py
@@ -8,17 +8,71 @@ def remove_titles(line):
     return line, None
 
 def process_text(line, counts):
+    if not any(keyword in line for keyword in ["تمدید شد ✅", "تمدید شد✅", "🟢"]):
+        return None  # فقط خطوط حاوی این عبارات پردازش می‌شوند
+    
+    if "شصد گیگ" in line:
+        if "سی روز" in line:
+            line = line.replace("✅", "✅  [78]")
+        elif "شصد روز" in line:
+            line = line.replace("✅", "✅  [90]")
+        elif "نود روز" in line:
+            line = line.replace("✅", "✅  [105]")
+        else:
+            line = line.replace("✅", "✅  [000000]")
+    
+    if "هفتاد گیگ" in line:
+        if "سی روز" in line:
+            line = line.replace("✅", "✅  [91]")
+        else:
+            line = line.replace("✅", "✅  [000000]")
+    
+    if "هشتاد گیگ" in line:
+        if "سی روز" in line:
+            line = line.replace("✅", "✅  [104]")
+        elif "شصد روز" in line:
+            line = line.replace("✅", "✅  [110]")
+        else:
+            line = line.replace("✅", "✅  [000000]")
+    
+    if "نود گیگ" in line:
+        if "سی روز" in line:
+            line = line.replace("✅", "✅  [117]")
+        elif "شصد روز" in line:
+            line = line.replace("✅", "✅  [125]")
+        elif "نود روز" in line:
+            line = line.replace("✅", "✅  [135]")
+        else:
+            line = line.replace("✅", "✅  [000000]")
+    
+    if "صد گیگ" in line:
+        if "سی روز" in line or "شصد روز" in line:
+            line = line.replace("✅", "✅  [130]")
+        elif "نود روز" in line:
+            line = line.replace("✅", "✅  [150]")
+        else:
+            line = line.replace("✅", "✅  [000000]")
+    
+    if "صد و بیست گیگ" in line:
+        if "سی روز" in line or "شصد روز" in line:
+            line = line.replace("✅", "✅  [156]")
+        elif "نود روز" in line:
+            line = line.replace("✅", "✅  [165]")
+        else:
+            line = line.replace("✅", "✅  [000000]")
+    
+    if "صد و پنجاه گیگ" in line:
+        if "سی روز" in line or "شصد روز" in line or "نود روز" in line:
+            line = line.replace("✅", "✅  [195]")
+        else:
+            line = line.replace("✅", "✅  [000000]")
+    
     replacements = {
         "ده گیگ": "✅  [25]",
         "بیست گیگ": "✅  [35]",
         "سی گیگ": "✅  [45]",
         "چهل گیگ": "✅  [55]",
         "پنجاه گیگ": "✅  [65]",
-        "شصد گیگ": "✅  [000000]",
-        "هفتاد گیگ": "✅  [000000]",
-        "هشتاد گیگ": "✅  [000000]",
-        "نود گیگ": "✅  [000000]",
-        "صد گیگ": "✅  [000000]",
         "صدو بیست گیگ": "✅  [000000]",
         "صدو پنجاه گیگ": "✅  [000000]"
     }
@@ -65,19 +119,17 @@ def main():
         if date:
             dates.append(date)
         
-        if not any(keyword in line for keyword in ["تمدید شد ✅", "تمدید شد✅", "🟢"]):
-            continue  # فقط خطوط حاوی این عبارات پردازش می‌شوند
-        
-        line = process_text(line, counts)
-        processed_lines.append(line)
-        
-        if "[000000]" in line:
-            review_lines.append(line)
-        
-        matches = re.findall(r"\[(\d+)\]", line)
-        for match in matches:
-            if match != "000000":
-                counts['sum'] += int(match)
+        processed_line = process_text(line, counts)
+        if processed_line:
+            processed_lines.append(processed_line)
+            
+            if "[000000]" in processed_line:
+                review_lines.append(processed_line)
+            
+            matches = re.findall(r"\[(\d+)\]", processed_line)
+            for match in matches:
+                if match != "000000":
+                    counts['sum'] += int(match)
     
     start_date = min(dates) if dates else "نامشخص"
     end_date = max(dates) if dates else "نامشخص"
